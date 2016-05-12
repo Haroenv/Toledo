9b0df7fb88ac7021e240501287cf5260764a1af7
Haroen Viaene <hello@haroen.me>
2016-05-12 11:14:34 +0200

    hamburger is for noobs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,13 @@
         <div class="container">
             <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <!-- No Hamburger -->
+               <!--  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-                </button>
+                </button> -->
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -32,40 +32,42 @@
                 </a>
             </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                    @if (!Auth::guest())
-                    @if (!strpos(Auth::user()->email, 'student'))
-                    <li><a href="{{ url('/admin') }}">Admin</a></li>
-                    @endif
-                    @endif
-                    <li><a href="{{ url('/help') }}">Help</a></li>
-                </ul>
+            <!-- no hamburger -->
+            <!-- <div class="collapse navbar-collapse" id="app-navbar-collapse"> -->
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                <li><a href="{{ url('/home') }}">Home</a></li>
+                @if (!Auth::guest())
+                @if (!strpos(Auth::user()->email, 'student'))
+                <li><a href="{{ url('/admin') }}">Admin</a></li>
+                @endif
+                @endif
+                <li><a href="{{ url('/help') }}">Help</a></li>
+            </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/user') }}"><i class="fa fa-btn fa-user"></i>{{ Auth::user()->name }}</a></li>
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/user') }}"><i class="fa fa-btn fa-user"></i>{{ Auth::user()->name }}</a></li>
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+            <!-- </div> -->
         </div>
     </nav>
+</div>
 
     @yield('content')
 
