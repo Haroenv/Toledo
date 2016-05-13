71e6390a0261f5888feab66460b765062a569195
Haroen Viaene <hello@haroen.me>
2016-05-13 07:38:23 +0200

    it's a me courses

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            @foreach ($courses as $course)
+            <div class="panel panel-default">
+                <div class="panel-heading">{{$course->name}}</div>
+
+                <div class="panel-body">
+                    {{$course->id}}
+                    {{$course}}
+                </div>
+            </div>
+            @endforeach
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
